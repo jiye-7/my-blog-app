35c808e26161ffc61281bcaae5bad65ee2c2c076
jiye-7 <jy522hi@gmail.com>
2024-02-09 02:13:30 +0900

    layout, project structure setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import { Link } from 'react-router-dom';
+import Router from './components/Router';
 
 function App() {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/posts'>Posts List</Link>
-        </li>
-        <li>
-          <Link to='/posts/:id'>Post Detail</Link>
-        </li>
-        <li>
-          <Link to='/posts/new'>Post New</Link>
-        </li>
-        <li>
-          <Link to='/posts/edit/:id'>Post Edit</Link>
-        </li>
-        <li>
-          <Link to='/profile'>Profile</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path='/' element={<div>Home Page</div>} />
-        <Route path='/posts' element={<div>Posts List Page</div>} />
-        <Route path='/posts/:id' element={<div>Post Detail Page</div>} />
-        <Route path='/posts/new' element={<div>Post New Page</div>} />
-        <Route path='/posts/edit/:id' element={<div>Post Edit Page</div>} />
-        <Route path='/profile' element={<div>Profile Page</div>} />
-        <Route path='*' element={<Navigate replace to='/' />} />
-      </Routes>
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
