970b3d17e438dd4b848a64314f0c43e60cf69bf4
jiye-7 <jy522hi@gmail.com>
2024-02-09 16:48:00 +0900

    className 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
           <Link to='/profile'>프로필</Link>
         </div>
       </header>
-      <div className='post-list'>Post list</div>
+      <div className='post__list'>Post list</div>
       <footer>
         <div>Menu 1</div>
         <div>Menu 2</div>
